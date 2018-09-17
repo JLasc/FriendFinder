c4f5d20fb27a17445b7041da77638b9bd46bc7a8
JLasc <lascaze.j@gmail.com>
2018-09-17 15:25:03 -0400

    friend api endpoint

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [{
+    name: 'Joe Schmoe',
+    profilePic: 'https://vignette.wikia.nocookie.net/spongebob/images/3/33/Patrick_Star.svg/revision/latest/scale-to-width-down/270?cb=20100724183918',
+    scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+    ]
+}]
+
+module.exports = friendList;
